255839b2f27ba6462adbf55a0efe6a4de489e7cb
Alex Harman <harmanator89@yahoo.com>
2018-10-10 22:24:15 -0400

    ValveStatus Color change
    Change valve open / closed color by clicking on it.

--- Assets/Scripts/ValveController.cs
+++ Assets/Scripts/ValveController.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class ValveController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public bool ValveStatus;
+    public Material[] ValveMaterials;
+
+    private GameObject UpstreamPart;
+    private GameObject DownstreamPart;
+    private Transform meeple;
+
+    MeshRenderer m_Renderer;
+
+    // Use this for initialization
+    void Start () {
+        meeple = this.gameObject.transform.GetChild(0);
+        UpstreamPart = this.gameObject.transform.GetChild(0).GetChild(0).gameObject;
+        DownstreamPart = this.gameObject.transform.GetChild(0).GetChild(1).gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
+        if (ValveStatus == true)
+        {
+            //Valve open
+            m_Renderer = UpstreamPart.GetComponent<MeshRenderer>();
+            m_Renderer.material = ValveMaterials[0];
+        }
+        else
+        {
+            //Valve closed
+            //Child = GameObject.Find("Cone").GetComponentsInChildren;
+            //Child = this.GetComponentInChildren;
+            m_Renderer = UpstreamPart.GetComponent<MeshRenderer>();
+            m_Renderer.material = ValveMaterials[1];
+        }
+
 	}
 
     void OnMouseDown()
     {
-        Debug.Log("Clicked on Valve");
+        ValveStatus = !ValveStatus;
     }
 }
